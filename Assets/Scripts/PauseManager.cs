4bb6f5adabe7c9246b930b93ede8b87d08bad620
feli-web <felipefunes2002@gmail.com>
2024-05-11 01:31:09 -0300

    Initial commit

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    public Image pause;
+    bool isPaused;
+    void Start()
+    {
+        isPaused = false;
+        pause.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(Pause());
+        }
+    }
+    public void PauseButton()
+    {
+        StartCoroutine(Pause());
+    }
+    public IEnumerator Pause()
+    {
+        isPaused = !isPaused;
+        yield return new WaitForSecondsRealtime(0.1f); 
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            pause.gameObject.SetActive(true);
+        }
+        if (!isPaused)
+        {
+            Time.timeScale = 1;
+            pause.gameObject.SetActive(false);
+        }
+    }
+    public void Menu(int destination)
+    {
+        SceneManager.LoadScene(destination);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    public float speed;
+    public float jumpForce;
+
+    public bool isGrounded;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        
+        Movement(x);
+        
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+    public void Movement( float xMov)
+    {
+            if (xMov < 0)
+            {
+                sr.flipX = true;
+            }
+            if (xMov > 0)
+            {
+                sr.flipX = false;
+            }
+
+            rb.velocity = new Vector2(xMov * speed, rb.velocity.y);
+    }
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
